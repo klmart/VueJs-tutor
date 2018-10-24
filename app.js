a9332238f8289d83d24c625c8b66669c7581a5a0
klmart <kulbaev.mart@gmail.com>
2018-10-24 15:12:29 +0600

    #14 used refs

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-Vue.component('greet', {
-  template: '<h2>{{title}}<button @click="changeTitle">Change title</button></h2>',
-  data: function() {
-    return {
-      title: 'Title template'
-    }
-  },
-  methods: {
-    changeTitle: function() {
-      this.title = 'Update title!'
-    }
-  },
-});
-
 new Vue({
   el: "#app-one",
-  data: {},
+  data: {
+    title: 'Title'
+  },
 
-  methods: {},
+  computed: {},
 
-  computed: {}
+  methods: {
+    changeTitle: function() {
+      console.log(this.$refs);
+      this.title = this.$refs.myInput.value
+    }
+  }
+
 });
