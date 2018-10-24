6e81adac33bc35774e73ca140705a783357b8524
klmart <kulbaev.mart@gmail.com>
2018-10-24 14:47:24 +0600

    #13 intro to components

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-var one = new Vue({
-  el: "#app-one",
-  data: {
-    title: 'Title one'
+Vue.component('greet', {
+  template: '<h2>{{title}}<button @click="changeTitle">Change title</button></h2>',
+  data: function() {
+    return {
+      title: 'Title template'
+    }
   },
+  methods: {
+    changeTitle: function() {
+      this.title = 'Update title!'
+    }
+  },
+});
+
+new Vue({
+  el: "#app-one",
+  data: {},
 
   methods: {},
 
-  computed: {
-    greet: function() {
-      return 'Hello from Vue one!'
-    }
-  }
+  computed: {}
 });
-
-var two = new Vue({
-  el: "#app-two",
-  data: {
-    title: 'Title two'
-  },
-
-  methods: {
-    changeTitleOne: function() {
-      one.title = 'Change from Vue two!'
-    }
-  },
-
-  computed: {
-    greet: function() {
-      return 'Hello from Vue two!'
-    }
-  }
-})
-
-two.title = 'Change from outside!'
