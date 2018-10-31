9e332ea0977549a4a158beb7fecae934105a943a
klmart <kulbaev.mart@gmail.com>
2018-10-31 14:40:56 +0600

    #19 used dynamic components

---
+++ src/components/Aut.vue
@@ -0,0 +1,25 @@
+<template>
+  <form method="post">
+    <h2>Authorization form</h2>
+    <p>
+      <label for="password">Your password:</label>
+      <input type="text" id="password" value="">
+    </p>
+    <p>
+      <label for="mail">Your E-mail:</label>
+      <input type="text" id="mail" value="">
+    </p>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+
+</style>
